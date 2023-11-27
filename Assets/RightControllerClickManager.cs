e138858521cfd2d539fa487c7b48b907714eb798
cjk8877 <dbseodnd356@gmail.com>
2023-11-27 20:38:54 +0900

    Bounce Ball Gun

--- Assets/RightControllerClickManager.cs
+++ Assets/RightControllerClickManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RightControllerClickManager : MonoBehaviour
 {
+    public GameObject prefab;
+    public float spawnSpeed = 5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class RightControllerClickManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
+        {
+            GameObject spawnBall = Instantiate(prefab, transform.position, Quaternion.identity);
+            Rigidbody spawnedBallRB = spawnBall.GetComponent<Rigidbody>();
+            spawnedBallRB.velocity = transform.forward * spawnSpeed;
+        }
     }
 }
